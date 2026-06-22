73632208d11fed775d978ce53a92e85d2bfd380b
fqzlr <a10275974@outlook.com>
2026-06-22 17:01:16 +0800

    save: pre-fix state before i18n missing keys repair

---
+++ check_keys.cjs
@@ -0,0 +1,22 @@
+﻿const fs = require("fs");
+
+// 1. Get all I18nKeys from source
+const src = fs.readFileSync("C:\\2\\dumplingandcakeblog\\src\\i18n\\i18nKey.ts", "utf8");
+const srcKeys = new Set();
+src.split("\n").forEach(line => {
+  const m = line.match(/^\t(\w+) = /);
+  if (m) srcKeys.add(m[1]);
+});
+
+// 2. Get all I18nKeys from target
+const tgt = fs.readFileSync("src/i18n/i18nKey.ts", "utf8");
+const tgtKeys = new Set();
+tgt.split("\n").forEach(line => {
+  const m = line.match(/^\t(\w+) = /);
+  if (m) tgtKeys.add(m[1]);
+});
+
+// 3. Find missing keys
+const missing = [...srcKeys].filter(k => !tgtKeys.has(k));
+console.log("Missing keys (" + missing.length + "):");
+missing.forEach(k => console.log("  " + k));
--- m13.cjs
+++
@@ -1,7 +0,0 @@
-﻿const fs = require("fs");
-let c = fs.readFileSync("src/config/siteConfig.ts", "utf8");
-const target = "calendar: true,";
-const replacement = "calendar: true,\n\t\t// 番剧页面开关\n\t\tbangumi: true,\n\t\t// 书架页面开关\n\t\tbooks: true,\n\t\t// 影视游戏页面开关\n\t\tmoviesGames: true,\n\t\t// 音乐页面开关\n\t\tmusicPage: true,\n\t\t// 更新日志页面开关\n\t\tchangelog: true,\n\t\t// 动态页面开关\n\t\tmoments: true,\n\t\t// 后台管理页面开关\n\t\tadmin: true,\n\t\t// 日常规划页面开关\n\t\tlifeRoutines: true,\n\t\t// 旅行足迹页面开关\n\t\tlifePlaces: true,\n\t\t// 笔记本页面开关\n\t\tlifeNotebooks: true,";
-c = c.replace(target, replacement);
-fs.writeFileSync("src/config/siteConfig.ts", c, "utf8");
-console.log("siteConfig pages updated");
